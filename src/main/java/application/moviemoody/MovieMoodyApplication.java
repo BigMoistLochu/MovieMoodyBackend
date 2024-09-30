096ecfdc3a2a7846511992f2b24416862c980ea8
BigMoistLochu <krok.konrad1@gmail.com>
2024-09-30 21:38:04 +0200

    added project to repo

---
+++ src/main/java/application/moviemoody/MovieMoodyApplication.java
@@ -0,0 +1,13 @@
+package application.moviemoody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMoodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieMoodyApplication.class, args);
+    }
+
+}
